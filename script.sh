ba78dc885296eca70b674da4af278f8066293f46
Shubham Agarwal <60534943+1996Shubh@users.noreply.github.com>
2020-06-22 10:00:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,23 @@
-sudo rm -rf /home/ubuntu/*
+sudo rm -rf /temp/*
 sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 pip3 install virtualenv
-cd /home/ubuntu/Chatapp_project/Chatapplication
+cd /home/Shubham/Myproject/Chatapplication
 virtualenv --python=python3 venv
-source /home/ubuntu/Chatapp_project/Chatapplication/venv/bin/activate
-cd /home/ubuntu/Chatapp_project/Chatapplication/chatapp
+source /home/Shubham/Myproject/Chatapplication/venv/bin/activate
+cd /home/Shubham/Myproject/Chatapplication/chatapp
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
-sudo chown ubuntu:ubuntu /home/ubuntu/Chatapp_project/Chatapplication/chatapp
-cd /home/ubuntu/Chatapp_project/Chatapplication/chatapp/
+sudo chown ubuntu:ubuntu /home/Shubham/Myproject/Chatapplication/chatapp
+cd /home/Shubham/Myproject/Chatapplication/chatapp/
 python3 manage.py collectstatic --noinput
-sudo cp /home/ubuntu/Chatapp_project/nginx/gunicorn.service /etc/systemd/system/
+sudo cp /home/Shubham/Myproject/nginx/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 sudo unlink /etc/nginx/sites-enabled/*
-sudo cp /home/ubuntu/Chatapp_project/nginx/fundoo /etc/nginx/sites-available/
+sudo cp /home/Shubham/Myproject/nginx/fundoo /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo nginx -t
 sudo rm /etc/nginx/sites-enabled/default
