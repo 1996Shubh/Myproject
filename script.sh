424f8f3df0f037b4d817423a41b4f4631621897a
Shubham Agarwal <60534943+1996Shubh@users.noreply.github.com>
2020-06-23 14:29:07 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,21 +2,22 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev nginx git -y
 sudo apt-get update
 pip3 install virtualenv
-cd Chatapplication
+script_dir=$(dirname "$0")
+cd $script_dir/Chatapplication
 virtualenv --python=python3 venv
-source Chatapplication/venv/bin/activate
-cd Chatapplication/chatapp
+source $script_dir/Chatapplication/venv/bin/activate
+cd $script_dir/Chatapplication/chatapp
 pip3 install -r requirements.txt
 pip3 install django bcrypt django-extensions
-sudo chown $USER:$USER Chatapplication/chatapp
-cd Chatapplication/chatapp/
+sudo chown $USER:$USER $script_dir/Chatapplication/chatapp
+cd $script_dir/Chatapplication/chatapp/
 python3 manage.py collectstatic --noinput
-sudo cp nginx/gunicorn.service /etc/systemd/system/
+sudo cp $script_dir/nginx/gunicorn.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start gunicorn
 sudo systemctl enable gunicorn
 sudo unlink /etc/nginx/sites-enabled/*
-sudo cp nginx/fundoo /etc/nginx/sites-available/
+sudo cp $script_dir/nginx/fundoo /etc/nginx/sites-available/
 sudo ln -s /etc/nginx/sites-available/fundoo /etc/nginx/sites-enabled
 sudo nginx -t
 sudo rm /etc/nginx/sites-enabled/default
